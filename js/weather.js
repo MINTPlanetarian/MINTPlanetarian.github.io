562303061fae348292ae877383b15e4c006b45f0
王启航 <2477758118@qq.com>
2022-11-28 21:28:53 +0800

    Site updated: 2022-11-28 21:28:50

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "F2A99B",
+    "tmpSize": "16",
+    "cityColor": "F2A99B",
+    "citySize": "16",
+    "aqiColor": "F2A99B",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "ch",
+    "borderRadius": "5",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "key": "9bda0018753b4a9ea45811622098ade9"
+  }
+}
